b6c9c2e371f36c66341e57a603c66a82285b424d
Legendary Hyper <141352658+LegendaryHyper@users.noreply.github.com>
2024-02-22 21:08:19 +0300

    Update main.py

--- Discord Bot/main.py
+++ Discord Bot/main.py
@@ -4,11 +4,14 @@ from discord.ext import commands
 import os, random
 import requests
 from googlesearch import search
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+load_dotenv()
 
+TOKEN = os.environ.get("TOKEN")
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
@@ -25,4 +28,4 @@ async def googlesearch(ctx, *search_msg: str):
     for URL in search(searchmessage, stop=5):
         url = URL
         await ctx.message.author.send(str(url))
-bot.run("TOKEN")
+bot.run(TOKEN)
